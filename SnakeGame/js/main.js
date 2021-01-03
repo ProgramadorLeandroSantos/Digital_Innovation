c75e7d1e27fa3a7bd76a1b3ec25c4db0cdb7cbcd
ProgramadorLeandroSantos <leandrosantosmartins1998@outlook.com>
2021-01-03 15:36:00 -0300

    finishing the game

--- SnakeGame/js/main.js
+++ SnakeGame/js/main.js
@@ -1,4 +1,5 @@
 let canvas = document.getElementById('snake');
+let scoreComponent = document.getElementById('score');
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
@@ -11,7 +12,6 @@ let food = {
     x: Math.floor(Math.random() * 15 + 1) * box ,
     y: Math.floor(Math.random() * 15 + 1) * box
 };
-
 
 function criarBG(){
     context.fillStyle = "purple";
@@ -40,11 +40,19 @@ function update(event){
 }
 
 function iniciarJogo(){
-    
+
     if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
     if(snake[0].x < 0 * box && direction == 'left') snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
     if(snake[0].y < 0 * box && direction == 'up') snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+             clearInterval(jogo);
+             let userDecision = window.confirm("Fim de jogo :(\nGostaria de recomeçar o jogo?")
+             if(userDecision) window.location.reload();
+        }   
+    }
 
     criarBG();
     criarCobrinha();
